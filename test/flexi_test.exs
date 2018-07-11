d1ae8b7fb112ad7b34d422e739461fa84add253c
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-11 20:06:37 +0200

    todo

--- test/flexi_test.exs
+++ test/flexi_test.exs
@@ -1,4 +1,16 @@
 defmodule FlexiTest do
   use ExUnit.Case
   doctest Flexi
+
+  test "first one" do
+    assert 1 == 1
+  end
+
+  test "second one" do
+    assert 2 == 2
+  end
+
+  test "third one" do
+    assert 2 == 2
+  end
 end
