448a654eeb3797b92a1c95b7846cc39bb6fbb6f7
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-12 13:19:20 +0200

    some mix meta info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Flexi.MixProject do
   use Mix.Project
+  @version "0.2.0"
 
   def project do
     [
       app: :flexi,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7-dev",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A flexible Elixir ExUnit test runner, perfect in combination with Cortex",
+      source_url: "https://github.com/happycodrz/flexi",
+      package: package()
     ]
   end
 
@@ -26,5 +30,13 @@ defmodule Flexi.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Roman Heinrich"],
+      licenses: ["MIT License"],
+      links: %{"Github" => "https://github.com/happycodrz/flexi"}
+    }
   end
 end
