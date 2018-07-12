6191947a2bf0320ad83968b11dcd7975e9e446c6
Roman Heinrich <roman.heinrich@gmail.com>
2018-07-12 22:07:18 +0200

    use edge version for now ex_docs, because on elixir edge the stable is broken

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule Flexi.MixProject do
     [
       {:cortex, "~> 0.5", only: [:dev, :test]},
       {:mockery, "~> 2.2", only: [:dev, :test], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      # {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ex_doc, github: "elixir-lang/ex_doc", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
